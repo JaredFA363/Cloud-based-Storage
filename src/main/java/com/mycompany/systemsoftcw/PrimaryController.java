ed9fc72aa2a914928238792ff0eca9d1bdbd4115
N0992216 <>
2023-01-21 16:42:13 +0000

    Buttons working to send to register and sign in pages

--- src/main/java/com/mycompany/systemsoftcw/PrimaryController.java
+++ src/main/java/com/mycompany/systemsoftcw/PrimaryController.java
@@ -2,8 +2,27 @@ package com.mycompany.systemsoftcw;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+
 
 public class PrimaryController {
+    
+    @FXML
+    private Button registerButton;
+    
+    @FXML 
+    private void switchToRegister(ActionEvent event) throws IOException{
+        App.setRoot("register");
+    }
+            
+    @FXML
+    private Button signInButton;
+    
+    @FXML 
+    private void switchToSignIn(ActionEvent event) throws IOException{
+        App.setRoot("signin");
+    }    
 
     @FXML
     private void switchToSecondary() throws IOException {
