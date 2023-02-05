54bb1a08088de801a947c77d9e7205dd4988151d
N0992216 <>
2023-02-05 15:48:41 +0000

    added profile section

--- src/main/java/com/mycompany/systemsoftcw/ProfileController.java
+++ src/main/java/com/mycompany/systemsoftcw/ProfileController.java
@@ -4,36 +4,16 @@
  */
 package com.mycompany.systemsoftcw;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
  *
  * @author ntu-user
  */
-public class MaininterfaceController implements Initializable {
-    
-    @FXML
-    private Button logoutButton;
-    
-    @FXML
-    private Button profileButton;
-    
-    @FXML 
-    private void logOut(ActionEvent event) throws IOException{
-        App.setRoot("primary");
-    }
-    
-    @FXML
-    private void GoToProfile(ActionEvent event) throws IOException{
-        App.setRoot("profile");
-    }
+public class ProfileController implements Initializable {
 
     /**
      * Initializes the controller class.
