bf543169d587fe40668c10f4c15f7e1afc483243
N1004932 <N1004932@my.ntu.ac.uk>
2023-04-18 16:21:09 +0000

    Updated Encryption but not complete

--- src/main/java/com/mycompany/systemsoftcw/Encryption.java
+++ src/main/java/com/mycompany/systemsoftcw/Encryption.java
@@ -12,6 +12,7 @@ import java.security.spec.KeySpec;
 import java.util.Arrays;
 import javax.crypto.*;
 import javax.crypto.spec.*;
+import java.sql.*;
 /**
  *
  * @author ntu-user
@@ -20,12 +21,31 @@ public class Encryption {
     private static final int KEY_SIZE = 256;
     private static final int SALT_SIZE = 10;
     private static final String ALGORITHM = "AES/CBC/PKCS5Padding";
+    private static final String DB_URL = "jdbc:sqlite:AccountDB.db";
     
-    public static void encrypt(File inputFile, File outputFile, String password) throws Exception{
+    public static void encrypt(Path inputFilePath, Path outputFilePath, String username) throws Exception{
+        String password = retrieveEncryptedPasswordFromDatabase(username);
         byte[] salt = generateSalt();
+        SecretKey key;
+        key = deriveKey(decryptPassword(password), salt);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        
+        byte[] inputBytes = Files.readAllBytes(inputFilePath);
+        byte[] encryptedBytes = cipher.doFinal(inputBytes);
+        
+        byte [] iv = cipher.getIV();
+        byte [] saltAndEncryptedBytes = new byte [SALT_SIZE + iv.length + encryptedBytes.length];
     }
 
     private static byte[] generateSalt() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
+
+    private static String retrieveEncryptedPasswordFromDatabase(String username) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+
 }
